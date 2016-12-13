c6146391b91a72f57b569752949487937364fd24
Parminder Singh <parminder@ezzie.in>
2016-12-13 14:12:54 +0530

    Dashboard

--- www/app/env.js
+++ www/app/env.js
@@ -15,22 +15,24 @@
     window.__env.password = "";
   }
   else if(environment === "qa") {
-    window.__env.uiServerUrl = 'http://ec2-54-169-94-251.ap-southeast-1.compute.amazonaws.com:8083';
-    window.__env.userServerUrl = 'http://ec2-54-169-94-251.ap-southeast-1.compute.amazonaws.com:8080';
-    window.__env.refDataUrl = 'http://ec2-54-169-94-251.ap-southeast-1.compute.amazonaws.com:8081/core';
-    window.__env.nodeJsUrl = 'http://ec2-54-169-94-251.ap-southeast-1.compute.amazonaws.com:8001/api';
-    window.__env.notificationServerUrl = 'http://ec2-54-169-94-251.ap-southeast-1.compute.amazonaws.com:8082/notification';
+    window.__env.uiServerUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8083';
+    window.__env.userServerUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8080';
+    window.__env.refDataUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8081/core';
+    window.__env.nodeJsUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8001/api';
+    window.__env.notificationServerUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8082/notification';
+    window.__env.dataServerUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8084';
     window.__env.baseUrl = '/';
     window.__env.enableDebug = true;
     window.__env.user = "user0@datiot.com";
     window.__env.password = "secret";
   }
   else {//DEFAULT - assume dev
-    window.__env.uiServerUrl = 'http://192.168.1.110:8083';
-    window.__env.userServerUrl = 'http://192.168.1.110:8080';
-    window.__env.refDataUrl = 'http://192.168.1.110:8081/core';
-    window.__env.nodeJsUrl = 'http://192.168.1.110:3000/api';
-    window.__env.notificationServerUrl = 'http://192.168.1.110:8082/notification';
+    window.__env.dataServerUrl = 'http://localhost:8084';
+    window.__env.uiServerUrl = 'http://localhost:8083';
+    window.__env.userServerUrl = 'http://localhost:8080';
+    window.__env.refDataUrl = 'http://localhost:8081/core';
+    window.__env.nodeJsUrl = 'http://localhost:3000/api';
+    window.__env.notificationServerUrl = 'http://localhost:8082/notification';
     window.__env.baseUrl = '/';
     window.__env.enableDebug = true;
     window.__env.user = "user0@datiot.com";
--- www/app/project/future-dashboard/dashboard.service.js
+++ www/app/project/future-dashboard/dashboard.service.js
@@ -8,97 +8,97 @@
   function dashboardFactory($http, __env) {
     var service = {};
 
-    service.getMainsData = function () { // TODO below need to be changed
-    var promise = $http.get('http://localhost:8084/dashboard/mainConsumption?siteId=58005c16e855161f7d388cf0')
-      .then(
+      service.getMainsData = function () { // TODO below need to be changed
+          var promise = $http.get(__env.dataServerUrl + '/dashboard/mainConsumption?siteId=58005c16e855161f7d388cf0')
+              .then(
 
-        function (response) {
-          return response;
-        },
-        function (response) {
-          return response;
-        });
-    return promise;
-  };
+                  function (response) {
+                      return response;
+                  },
+                  function (response) {
+                      return response;
+                  });
+          return promise;
+      };
 
-  service.getGensetData = function () { // TODO below need to be changed
-    var promise = $http.get('http://localhost:8084/dashboard/dgConsumption?siteId=58005c16e855161f7d388cf0')
-      .then(
+      service.getGensetData = function () { // TODO below need to be changed
+          var promise = $http.get(__env.dataServerUrl +'/dashboard/dgConsumption?siteId=58005c16e855161f7d388cf0')
+              .then(
 
-        function (response) {
-          return response;
-        },
-        function (response) {
-          return response;
-        });
-    return promise;
-  };
+                  function (response) {
+                      return response;
+                  },
+                  function (response) {
+                      return response;
+                  });
+          return promise;
+      };
 
-  service.getActiveHourData = function () { // TODO below need to be changed
-    var promise = $http.get('http://localhost:8084/dashboard/currentActive?siteId=58005c16e855161f7d388cf0')
-      .then(
+      service.getActiveHourData = function () { // TODO below need to be changed
+          var promise = $http.get(__env.dataServerUrl + '/dashboard/currentActive?siteId=58005c16e855161f7d388cf0')
+              .then(
 
-        function (response) {
-          return response;
-        },
-        function (response) {
-          return response;
-        });
-    return promise;
-  };
+                  function (response) {
+                      return response;
+                  },
+                  function (response) {
+                      return response;
+                  });
+          return promise;
+      };
 
-  service.getEnergyData = function () { // TODO below need to be changed
-    var promise = $http.get('http://localhost:8084/dashboard/dgMainHistogram?siteId=58005c16e855161f7d388cf0')
-      .then(
+      service.getEnergyData = function () { // TODO below need to be changed
+          var promise = $http.get(__env.dataServerUrl + '/dashboard/dgMainHistogram?siteId=58005c16e855161f7d388cf0')
+              .then(
 
-        function (response) {
-          return response;
-        },
-        function (response) {
-          return response;
-        });
-    return promise;
-  };
+                  function (response) {
+                      return response;
+                  },
+                  function (response) {
+                      return response;
+                  });
+          return promise;
+      };
 
-  service.getTemperatureData = function () { // TODO below need to be changed
-    var promise = $http.get('http://localhost:8084/dashboard/temperature?siteId=58005c16e855161f7d388cf0')
-      .then(
+      service.getTemperatureData = function () { // TODO below need to be changed
+          var promise = $http.get(__env.dataServerUrl + '/dashboard/temperature?siteId=58005c16e855161f7d388cf0')
+              .then(
 
-        function (response) {
-          return response;
-        },
-        function (response) {
-          return response;
-        });
-    return promise;
-  };
+                  function (response) {
+                      return response;
+                  },
+                  function (response) {
+                      return response;
+                  });
+          return promise;
+      };
 
 
-  service.getSupplyData = function () { // TODO below need to be changed
-    var promise = $http.get('http://localhost:8084/dashboard/parameters?siteId=58005c16e855161f7d388cf0')
-      .then(
+      service.getSupplyData = function () { // TODO below need to be changed
+          var promise = $http.get(__env.dataServerUrl + '/dashboard/parameters?siteId=58005c16e855161f7d388cf0')
+              .then(
 
-        function (response) {
-          return response;
-        },
-        function (response) {
-          return response;
-        });
-    return promise;
-  };
+                  function (response) {
+                      return response;
+                  },
+                  function (response) {
+                      return response;
+                  });
+          return promise;
+      };
 
-  service.getAssetData = function () { // TODO below need to be changed
-    var promise = $http.get('http://localhost:8084/dashboard/acStatus?siteId=58005c16e855161f7d388cf0')
-      .then(
+      service.getAssetData = function () { // TODO below need to be changed
+          var promise = $http.get(__env.dataServerUrl + '/dashboard/acStatus?siteId=58005c16e855161f7d388cf0')
+              .then(
 
-        function (response) {
-          return response;
-        },
-        function (response) {
-          return response;
-        });
-    return promise;
-  };
+                  function (response) {
+                      return response;
+                  },
+                  function (response) {
+                      return response;
+                  });
+          return promise;
+      };
 
     service.getData = function () { // TODO below need to be changed
       var promise = $http.get('/data/getData.json')
