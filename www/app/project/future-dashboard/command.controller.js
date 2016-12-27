1a587f4889f1ce6f1533ce6fe17e15279d23fe42
Paras Miglani <paras@datioit.com>
2016-12-27 17:08:45 +0530

    Changes done

--- www/app/project/future-dashboard/command.controller.js
+++ www/app/project/future-dashboard/command.controller.js
@@ -5,9 +5,9 @@
     .module('app.dashboard')
     .controller('CommandController', CommandController);
 
-  CommandController.$inject = ['$state'];
+  CommandController.$inject = ['$state', 'dashboardFactory', 'logger'];
   /* @ngInject */
-  function CommandController($state) {
+  function CommandController($state, dashboardFactory, logger) {
     var vm = this;
     vm.temp = {};
 
@@ -15,8 +15,60 @@
 
     function activate() {
 
-      $state.go('app.command');    
+      dashboardFactory.getAssetInfo().then(function (response) {
+        if (response.status == 200) {
+          vm.master = angular.copy(response.data);
+          console.log(response.data)
+          if(vm.master.AC1.disabled || vm.master.AC2.disabled || vm.master.AC3.disabled || vm.master.Signage.disabled){
+            vm.disableTime = false;
+          }
+          if(vm.master.AC1.status == 1.00){
+            vm.master.AC1.status = true;
+          }
+          else{
+            vm.master.AC1.status = false;
+          }
+          if(vm.master.AC2.status == 1.00){
+            vm.master.AC2.status = true;
+          }
+          else{
+            vm.master.AC2.status = false;
+          }
+          if(vm.master.AC3.status == 1.00){
+            vm.master.AC3.status = true;
+          }
+          else{
+            vm.master.AC3.status = false;
+          }
+          if(vm.master.Signage.status == 1.00){
+            vm.master.Signage.status = true;
+          }
+          else{
+            vm.master.Signage.status = false;
+          }
+          console.log(vm.master)
+          vm.control = angular.copy(vm.master);
+
+        }
+        else if (response.status == 404) {
+          logger.error('User not found', 'error');
+          console.error(response);
+        }
+        else if (response.status == -1) {
+          logger.error('Network Error', 'error');
+          console.error(response);
+        }
+        else {
+          logger.error('Backend error', 'error');
+          console.error(response);
+        }
+      });
+      // $state.go('app.command');    
     }
   }
 })();
+
+
+
+
 
