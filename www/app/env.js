afbf498303347ae396bda7598ab0c56711616540
ananya-17 <ananya@datiot.com>
2016-12-28 14:41:44 +0530

    in progress

--- www/app/env.js
+++ www/app/env.js
@@ -4,18 +4,22 @@
 (function (window) {
   window.__env = window.__env || {};
 
-  var environment = "dev"; //can be dev, test or prod.
+  var environment = "prod"; //can be dev, test or prod.
 
   //environment specific constants
   if(environment === "prod") {
-    window.__env.uiServerUrl = 'http://localhost:8081/core/';
+    window.__env.uiServerUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8083';
+    window.__env.userServerUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8080';
+    window.__env.refDataUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8081/core';
+    window.__env.nodeJsUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8001/api';
+    window.__env.notificationServerUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8082/notification';
+    window.__env.dataServerUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8084';
     window.__env.baseUrl = '/';
     window.__env.enableDebug = true;
-    window.__env.user = "";
+    window.__env.user = "user0@datiot.com";
     window.__env.password = "";
   }
   else if(environment === "qa") {
-    window.__env.dataServerUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8084';
     window.__env.uiServerUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8083';
     window.__env.userServerUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8080';
     window.__env.refDataUrl = 'http://ec2-54-251-154-20.ap-southeast-1.compute.amazonaws.com:8081/core';
@@ -25,7 +29,7 @@
     window.__env.baseUrl = '/';
     window.__env.enableDebug = true;
     window.__env.user = "user0@datiot.com";
-    window.__env.password = "secret";
+    window.__env.password = "";
   }
   else {//DEFAULT - assume dev
     window.__env.dataServerUrl = 'http://localhost:8084';
