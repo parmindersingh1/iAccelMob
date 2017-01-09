8dd58a0d35c2d2ca21e5001f2d7c10497bc6ca73
ananya-17 <ananya@datiot.com>
2017-01-09 11:24:13 +0530

    env.js updated

--- www/app/env.js
+++ www/app/env.js
@@ -6,12 +6,12 @@
 
   //environment specific constants
   if(environment === "prod") {
-    window.__env.uiServerUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8083';
-    window.__env.userServerUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8080';
-    window.__env.refDataUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8081/core';
+    window.__env.uiServerUrl = 'http://ec2-54-85-125-209.compute-1.amazonaws.com:8086/ui';
+    window.__env.userServerUrl = 'http://ec2-54-85-125-209.compute-1.amazonaws.com:8086/auth';
+    window.__env.refDataUrl = 'http://ec2-54-85-125-209.compute-1.amazonaws.com:8086/core';
     window.__env.nodeJsUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8001/api';
-    window.__env.notificationServerUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8082/notification';
-    window.__env.dataServerUrl = 'http://ec2-52-90-74-144.compute-1.amazonaws.com:8084';
+    window.__env.notificationServerUrl = 'http://ec2-54-85-125-209.compute-1.amazonaws.com:8086/notification';
+    window.__env.dataServerUrl = 'http://ec2-54-85-125-209.compute-1.amazonaws.com:8086/dataProcessor';
     window.__env.baseUrl = '/';
     window.__env.enableDebug = true;
     window.__env.user = "user0@datiot.com";
